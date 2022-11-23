1e5289e14511a581f67f45b7cd347dceafcfbea8
refracc <40345422@live.napier.ac.uk>
2022-11-23 18:07:29 +0000

    .

--- src/main/java/me/sanderson/ea/Algorithm.java
+++ src/main/java/me/sanderson/ea/Algorithm.java
@@ -1,15 +1,16 @@
 package me.sanderson.ea;
 
 import me.sanderson.ea.data.Problem;
-Added bas import org.jetbrains.annotations.Contract;
+import org.jetbrains.annotations.Contract;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.ArrayList;
-import java.util.Comparator;
 import java.util.List;
-import java.util.function.Supplier;
 import java.util.stream.Collectors;
 
+/**
+ * Output should be with parallel coordinates :)
+ */
 public class Algorithm {
 
     public static void main(String[] args) {
@@ -24,6 +25,22 @@ public class Algorithm {
         population.forEach(System.out::println);
     }
 
+    /*
+        d888888b d8b   db d888888b d888888b d888888b  .d8b.  db      d888888b .d8888.  .d8b.  d888888b d888888b  .d88b.  d8b   db
+          `88'   888o  88   `88'   `~~88~~'   `88'   d8' `8b 88        `88'   88'  YP d8' `8b `~~88~~'   `88'   .8P  Y8. 888o  88
+           88    88V8o 88    88       88       88    88ooo88 88         88    `8bo.   88ooo88    88       88    88    88 88V8o 88
+           88    88 V8o88    88       88       88    88~~~88 88         88      `Y8b. 88~~~88    88       88    88    88 88 V8o88
+          .88.   88  V888   .88.      88      .88.   88   88 88booo.   .88.   db   8D 88   88    88      .88.   `8b  d8' 88  V888
+        Y888888P VP   V8P Y888888P    YP    Y888888P YP   YP Y88888P Y888888P `8888Y' YP   YP    YP    Y888888P  `Y88P'  VP   V8P
+     */
+
+    /**
+     * Augmented initialisation.
+     * Initialise a significantly larger population of {@link Chromosome}s and
+     * take the top {@code n} candidates from this population to pre-seed the
+     * population with good results.
+     * @return A population pre-seeded with good(ish) results.
+     */
     @Contract(pure = true)
     private static @NotNull List<Chromosome> augmented() {
         List<Chromosome> pop = new ArrayList<>();
@@ -34,9 +51,46 @@ public class Algorithm {
         return pop.stream().sorted().limit(Parameters.POPULATION_SIZE).collect(Collectors.toList());
     }
 
+    /**
+     * The procedure for evaluating a {@link List} of {@link Chromosome}s.
+     * @param population The population of {@link Chromosome}s.
+     */
     private static void evaluate(@NotNull List<Chromosome> population) {
         for (Chromosome c : population) {
             c.getFitness();
         }
+    }
+
+    /**
+     * The function to retrieve the best {@link Chromosome} from the population.
+     * @param population The population of {@link Chromosome}s.
+     * @return The best {@link Chromosome} from the population based on fitness.
+     */
+    private static Chromosome getBestChromosome(@NotNull List<Chromosome> population) {
+        Chromosome best = null;
+
+        for (Chromosome c : population) {
+            if (best == null || c.getFitness() < best.getFitness()) {
+                best = c.copy();
+            }
+        }
+        return best;
+    }
+
+    /**
+     * Activation function selection.
+     * Select the activation function for the evolutionary algorithm.
+     * @param v The value to be passed in to the activation function.
+     * @return The result of the activation function.
+     */
+    private static double activation(double v) {
+        return switch (Parameters.ACTIVATION) {
+            case LEAKY_RELU -> (v > 0) ? v : v / 100;
+            case RELU -> (v > 0.0) ? v : -1.0;
+            case STEP -> (v <= 0) ? -1.0 : 1.0;
+            case SWISH -> v * (1 / (1 + Math.exp(-v)));
+            case TANH -> (v < -20.0) ? -1.0 : (v > 20.0) ? 1.0 : Math.tanh(v);
+            case ELU -> (v > 0) ? v : (Math.exp(v) - 1) / 10;
+        };
     }
 }
--- src/main/java/me/sanderson/ea/Chromosome.java
+++ src/main/java/me/sanderson/ea/Chromosome.java
@@ -14,7 +14,7 @@ import java.util.List;
  * @version 0.0.1
  * @since 2022-11-22
  */
-public class Chromosome {
+public class Chromosome implements Comparable {
 
     private final List<Point> points;
     private double fitness;
@@ -43,6 +43,18 @@ public class Chromosome {
     public Chromosome(List<Point> points) {
         this();
         this.points.addAll(points);
+    }
+
+    /**
+     * A constructor creating a new {@link Chromosome} with a {@link List} of {@link Point}s.
+     * This constructor is typically only used with the {@link Chromosome#copy()} method.
+     * @param points The {@link List} of {@link Point}s to add to the {@link Chromosome}.
+     * @param fitness The fitness of the individual.
+     */
+    public Chromosome(List<Point> points, double fitness) {
+        this();
+        this.points.addAll(points);
+        this.fitness = fitness;
     }
 
     /**
@@ -113,6 +125,35 @@ public class Chromosome {
         return this;
     }
 
+    public Chromosome copy() {
+        return new Chromosome(this.points, this.fitness);
+    }
+
+    /**
+     * Returns a string representation of the object.
+     * @apiNote
+     * In general, the
+     * {@code toString} method returns a string that
+     * "textually represents" this object. The result should
+     * be a concise but informative representation that is easy for a
+     * person to read.
+     * It is recommended that all subclasses override this method.
+     * The string output is not necessarily stable over time or across
+     * JVM invocations.
+     * @implSpec
+     * The {@code toString} method for class {@code Object}
+     * returns a string consisting of the name of the class of which the
+     * object is an instance, the at-sign character `{@code @}', and
+     * the unsigned hexadecimal representation of the hash code of the
+     * object. In other words, this method returns a string equal to the
+     * value of:
+     * <blockquote>
+     * <pre>
+     * getClass().getName() + '@' + Integer.toHexString(hashCode())
+     * </pre></blockquote>
+     *
+     * @return  a string representation of the object.
+     */
     @Override
     public String toString() {
         final StringBuilder sb = new StringBuilder("Chromosome {\n");
@@ -128,5 +169,43 @@ public class Chromosome {
         sb.append("\t}\n");
         sb.append("Fitness: ").append(getFitness());
         return sb.toString();
+    }
+
+    /**
+     * Compares this object with the specified object for order.  Returns a
+     * negative integer, zero, or a positive integer as this object is less
+     * than, equal to, or greater than the specified object.
+     *
+     * <p>The implementor must ensure {@link Integer#signum
+     * signum}{@code (x.compareTo(y)) == -signum(y.compareTo(x))} for
+     * all {@code x} and {@code y}.  (This implies that {@code
+     * x.compareTo(y)} must throw an exception if and only if {@code
+     * y.compareTo(x)} throws an exception.)
+     *
+     * <p>The implementor must also ensure that the relation is transitive:
+     * {@code (x.compareTo(y) > 0 && y.compareTo(z) > 0)} implies
+     * {@code x.compareTo(z) > 0}.
+     *
+     * <p>Finally, the implementor must ensure that {@code
+     * x.compareTo(y)==0} implies that {@code signum(x.compareTo(z))
+     * == signum(y.compareTo(z))}, for all {@code z}.
+     *
+     * @param o the object to be compared.
+     * @return a negative integer, zero, or a positive integer as this object
+     * is less than, equal to, or greater than the specified object.
+     * @throws NullPointerException if the specified object is null
+     * @throws ClassCastException   if the specified object's type prevents it
+     *                              from being compared to this object.
+     * @apiNote It is strongly recommended, but <i>not</i> strictly required that
+     * {@code (x.compareTo(y)==0) == (x.equals(y))}.  Generally speaking, any
+     * class that implements the {@code Comparable} interface and violates
+     * this condition should clearly indicate this fact.  The recommended
+     * language is "Note: this class has a natural ordering that is
+     * inconsistent with equals."
+     */
+    @Override
+    public int compareTo(@NotNull Object o) {
+        Chromosome c = (Chromosome) o;
+        return Double.compare(this.getFitness(), c.getFitness());
     }
 }
